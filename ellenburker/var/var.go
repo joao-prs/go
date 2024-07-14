fa29bd8809d3df8fbeda9a970821d3d16c2355c8
jpedro <kandrocmatunein@gmail.com>
2024-07-14 11:08:47 -0300

    learning 1

---
+++ ellenburker/var/var.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt" /*int, string,bool*/
+)
+/*
+	a variavel declarada em um bloco de codigo,
+	é undefined em outro, ou seja, se ela existe
+	em qualquercoisa() ela nao existe em main()	
+*/
+var z int
+
+func qualquercoisa(x int) {
+	fmt.Println(x)
+}
+
+func main() {
+	
+	y := 10
+	qualquercoisa(y)
+
+	/* a atribuição dessa var z so pode ser feita
+	dentro de um codeblock, como o caso a baixo */
+	z = 10
+	Println(z)	
+}
---
+++ ellenburker/zero/zero.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+var a int
+var b float64
+var c string
+var d bool
+
+
+func main() {
+	x := 10
+	fmt.Printf("%v, %T\n", x, x)
+
+	
+	fmt.Printf("%v, %T\n", a, a)
+	fmt.Printf("%v, %T\n", b, b)
+	fmt.Printf("%v, %T\n", c, c)
+	fmt.Printf("%v, %T\n", d, d)
+}
