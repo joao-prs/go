df6774801979d44c6845e3d69af70949978e0276
jpedro <kandrocmatunein@gmail.com>
2024-08-01 09:36:59 -0300

    check & start packge

--- check-package-installed/main.go
+++ check-package-installed/main.go
@@ -5,14 +5,14 @@ import (
 	"os/exec"
 )
 
-// verifica se o pacote ta instalado
+// check pacote instalado
 func isPackageInstalled(pkgName string) bool {
 	cmd := exec.Command("dpkg", "-s", pkgName)
 	err := cmd.Run()
 	return err == nil
 }
 
-// instalar pacote
+// instala pacote
 func installPackage(pkgName string) error {
 	cmd := exec.Command("sudo", "apt-get", "install", "-y", pkgName)
 	cmd.Stdout = nil
@@ -20,9 +20,35 @@ func installPackage(pkgName string) error {
 	return cmd.Run()
 }
 
+// inicia serviço
+func startService(serviceName string) error {
+	cmd := exec.Command("sudo", "systemctl", "start", serviceName)
+	cmd.Stdout = nil
+	cmd.Stderr = nil
+	return cmd.Run()
+}
+
+// reinicia serviço
+func restartService(serviceName string) error {
+	cmd := exec.Command("sudo", "systemctl", "restart", serviceName)
+	cmd.Stdout = nil
+	cmd.Stderr = nil
+	return cmd.Run()
+}
+
+// habilita serviço
+func enableService(serviceName string) error {
+	cmd := exec.Command("sudo", "systemctl", "enable", serviceName)
+	cmd.Stdout = nil
+	cmd.Stderr = nil
+	return cmd.Run()
+}
+
 
 func main() {
+	
 	packages := []string{"apache2", "php8.0"}
+	services := []string{"apache2"}
 
 	for _, pkg := range packages {
 		if isPackageInstalled(pkg) {
@@ -35,6 +61,24 @@ func main() {
 			} else {
 				fmt.Printf("Pacote %s instalado com sucesso.\n", pkg)
 			}
+		}
+	}
+
+	for _, svc := range services {
+		fmt.Printf("Habilitando o serviço %s...\n", svc)
+		err := enableService(svc)
+		if err != nil {
+			fmt.Printf("Falha ao habilitar o serviço %s: %v\n", svc, err)
+		} else {
+			fmt.Printf("Serviço %s habilitado com sucesso.\n", svc)
+		}
+
+		fmt.Printf("Reiniciando o serviço %s...\n", svc)
+		err = restartService(svc)
+		if err != nil {
+			fmt.Printf("Falha ao reiniciar o serviço %s: %v\n", svc, err)
+		} else {
+			fmt.Printf("Serviço %s reiniciado com sucesso.\n", svc)
 		}
 	}
 }
